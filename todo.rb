cdf65854e3d0164a451b5fbf7f95874dce48b223
denzel <pressey90@gmail.com>
2014-04-06 17:10:57 -0400

    add 404 page, add todo form

--- todo.rb
+++ todo.rb
@@ -6,3 +6,19 @@ DataMapper::Logger.new($stdout, :debug)
 
 DataMapper.setup(:default, 'sqlite://#{Dir.pwd}/database.db')
 
+get '/' do
+	erb :index
+end
+
+post '/' do
+	params[:input].upcase + "Say Something....I'M GIVING UP ON YOU"
+end
+
+get '/about' do
+	"this is the about me section"
+end
+
+not_found do
+	status 404
+	erb :404
+end
